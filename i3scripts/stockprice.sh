155a2a212bd5a27d6b35e7c3cdc93642369956bc
jonson mccready <for44jtime@protonmail.com>
2021-09-16 18:52:47 -0400

    switched to amd instead of gme

--- i3scripts/stockprice.sh
+++ i3scripts/stockprice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gmeprice=$(curl -s https://query1.finance.yahoo.com/v7/finance/quote\?symbols\=gme | sed 's/,/\n/g' | grep "regularMarketPrice" | cut -d ':' -f2)
-echo "💲GME: $gmeprice"
+price=$(curl -s https://query1.finance.yahoo.com/v7/finance/quote\?symbols\=amd | sed 's/,/\n/g' | grep "regularMarketPrice" | cut -d ':' -f2)
+echo "💲AMD: $price"
 
 case $BLOCK_BUTTON in
     1) "$TERMINAL" -e mop ;;
