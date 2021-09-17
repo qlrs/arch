beda55c5726e235f14b6fc01369ff56becced9d7
jonson mccready <for44jtime@protonmail.com>
2021-09-17 18:49:34 -0400

    removed moon phase

--- i3scripts/weather.sh
+++ i3scripts/weather.sh
@@ -6,7 +6,6 @@ curl -s https://wttr.in/Myrtle_Beach\?format=j1 > /tmp/weather_forecast
 
 getforecast() {
     temp="$(jq -r ."current_condition[0].temp_F" /tmp/weather_forecast)"
-    moonphase="$(jq -r ."weather[0]|.astronomy[0]|.moon_phase" /tmp/weather_forecast)"
     condition="$(jq -r ."current_condition[0]|.weatherDesc[0]|.value" /tmp/weather_forecast)"
 }
 
@@ -19,7 +18,7 @@ displayforecast () {
         *) icon="☀️";;
     esac
 
-    [ "$(echo $moonphase | grep "Full Moon")" ] && echo "🌕 $icon $temp℉ " || echo "$icon $temp℉ "
+    echo "$icon $temp℉ "
 }
 
 case $BLOCK_BUTTON in
