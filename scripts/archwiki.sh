a38a1207b7629f66f83612f8d97a2f13b6d1f955
jonson mccready <for44jtime@protonmail.com>
2021-09-18 18:57:37 -0400

    shellcheck

--- scripts/archwiki.sh
+++ scripts/archwiki.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-#
+
 [ ! -d "/usr/share/doc/arch-wiki" ] && notify-send "arch-wiki-docs package needed..." && exit 1
-selection=$(ls /usr/share/doc/arch-wiki/html/en | sed 's/\.html//g' | dmenu -fn "Liberation Mono-13" -nb "#000000" -nf "#ffffff" -sb "#0066ff" -i -l 20)
+
+selection=$(find /usr/share/doc/arch-wiki/html/en -printf "%f\n" | dmenu -fn "Liberation Mono-13" -nb "#000000" -nf "#ffffff" -sb "#0066ff" -i -l 20)
+
 [ -z "$selection" ] && notify-send "Nothing selected... exiting" && exit 1
-firefox "/usr/share/doc/arch-wiki/html/en/$selection.html"
+
+firefox "/usr/share/doc/arch-wiki/html/en/$selection"
--- scripts/replacewebsitepics.sh
+++ scripts/replacewebsitepics.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#Requires text file with names of pictures that you want, seperated by newlines,
-#gives output that you can paste into your website html.
+# Requires text file with names of pictures that you want, seperated by newlines,
+# gives output that you can paste into your website html.
 
 
 [ -z "$1" ] && notify-send "Please enter text file..." && exit
@@ -12,7 +12,7 @@ thetext=$(printf "<a href="pics/title1" target"_blank">
 
 
 while IFS= read -r line; do
-    realname=$(echo "$line")
+    realname="$line"
     thumbname=$(echo "$realname" | sed 's/^/thumbnail-/g')
     echo "$thetext" | sed "s/title1/$realname/;s/title2/$thumbname/" >> correctedforwebsite.txt
 done < "$1"
