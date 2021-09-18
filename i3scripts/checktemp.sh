48478a5577f6c31def42e70205920cc0bfaa229e
jonson mccready <for44jtime@protonmail.com>
2021-09-17 20:35:36 -0400

    shellcheck

--- i3scripts/checktemp.sh
+++ i3scripts/checktemp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 temp=$(sensors | awk '/^Package id 0/ {print $4}')
-echo "🌡️ "$temp
+echo "🌡️ $temp"
 
 case $BLOCK_BUTTON in
     3) notify-send -t 8000 "$(sensors | sed -n '5,18p' | sed 's/([^)]*)//g')"
