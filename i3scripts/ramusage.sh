bb35160cec4f89fbcb8e0b5179740fc5c155d1cb
jonson mccready <for44jtime@protonmail.com>
2021-09-19 21:42:17 -0400

    removed sed from notification

--- i3scripts/ramusage.sh
+++ i3scripts/ramusage.sh
@@ -7,5 +7,5 @@ echo "🐏 $ram"
 
 case $BLOCK_BUTTON in
     1) "$TERMINAL" -e "$tp";;
-    3) notify-send -t 8000 "$(ps axch -o cmd:10,%mem --sort=-%mem | sed 's/$/%/g' | head)" ;;
+    3) notify-send -t 8000 "$(ps axch -o command:10,%mem,pid --sort=-%mem | head)" ;;
 esac
