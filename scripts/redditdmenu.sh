ef98a762418b743535b7817f12dfb999be577794
jonson mccready <for44jtime@protonmail.com>
2021-09-28 19:00:07 -0400

    added privacyguides

--- scripts/redditdmenu.sh
+++ scripts/redditdmenu.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
 selection=$(printf "stallmanwasright
+mousereview
 livestreamfail
+valorant
+valorantcompetitive
 bash
 onions
 tor
+imaginarysliceoflife
 commandline
 thinkpad
 cyberpunk
 privacytoolsIO
+privacyguides
 privacy
+destiny
 linux
 politics
+linuxmasterrace
+linuxquestions
 archlinux
 artixlinux
 voidlinux" | dmenu -fn "Liberation Mono-13" -nb "#282a36" -nf "#f8f8f2" -sb "#bd93f9" -i -l 20)
 
-
-url="https://teddit.net/r/$selection"
-
+url="https://teddit.net/r/""$selection"
 
 [ -z "$selection" ] && notify-send "Nothing selected..." && exit
 
-
 #torornot=$(printf "Yes\nNo" | dmenu -i -fn "Liberation Mono-13" -l 2 -p "Would you like to use tor browser?")
-
 
 #if [ "$torornot" = "Yes" ]; then
 #    /home/dan/.local/share/torbrowser/tbb/x86_64/tor-browser_en-US/Browser/start-tor-browser "$url" &
@@ -33,7 +37,5 @@ url="https://teddit.net/r/$selection"
 #else
 #    qutebrowser "$url" &
 #fi
-
-
 notify-send "Opening r/$selection"
 $BROWSER "$url"
