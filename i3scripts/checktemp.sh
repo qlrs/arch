456e36b06ad1fb7b4a2ae1c7fefd417a456597e8
jonson mccready <for44jtime@protonmail.com>
2021-10-16 18:31:37 -0400

    left click -> show cpu program usage

--- i3scripts/checktemp.sh
+++ i3scripts/checktemp.sh
@@ -4,5 +4,6 @@ temp="$(sensors | awk '/^Package id 0/ {print $4}')"
 echo "🌡️ $temp"
 
 case $BLOCK_BUTTON in
-    3) notify-send -t 8000 "$(sensors | awk '/Core/ {print $1,$2,$3}')"
+    1) notify-send -t 8000 "$(ps axch -o command:10,%cpu,pid --sort=-%cpu | head)" ;;
+    3) notify-send -t 8000 "$(sensors | awk '/Core/ {print $1,$2,$3}')" ;;
 esac
