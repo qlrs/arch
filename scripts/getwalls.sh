6a2ccf25e0a066f19cb7c8c145e21f1e9384fd48
jonson mccready <for44jtime@protonmail.com>
2021-10-16 20:33:43 -0400

    complete rework

--- scripts/getwalls.sh
+++ scripts/getwalls.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
-#Give url of picture or a spaced list such as
-#url name
-
+# Give url of picture or list with $url $name
 
 [ -z "$1" ] && notify-send "Please enter a url or list" && exit
 
+getone() {
+    echo "What do you want the file to be called? "; read -r name
+    while true ; do
+        if ! [ -e "/$HOME/walls/$name" ] ; then
+            wget -q "$1" -O "/$HOME/walls/$name" 2> /dev/null
+            echo "Downloaded $1 as $name"
+            break
+        else
+            echo "That file exists, please give another name " ; read -r name
+        fi
+    done
+}
+
+getmultiple() {
+    while read -r url name; do
+        while true ; do
+            if ! [ -e "/$HOME/walls/$name" ] ; then
+                wget -q "$url" -O "/$HOME/walls/$name" 2> /dev/null
+                break
+            else
+                echo "The file $name exists, please change the name."
+                break
+            fi
+        done
+    done < "$1"
+}
 
 if echo "$1" | grep -q "http"; then
-    echo "What do you want the file to be called? "; read -r name
-    wget "$1" -O "$name"
-    mv "$name" "$HOME"/walls
+    getone "$@"
 else
-    while read -r url name; do
-        wget "$url" -O "$name"
-        mv "$name" "$HOME"/walls
-    done < "$1"
+    getmultiple "$@"
 fi
