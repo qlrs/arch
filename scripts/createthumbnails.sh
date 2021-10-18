2b3a157cf6aa119981d2d87650a36cd40e4d7785
jonson mccready <for44jtime@protonmail.com>
2021-10-18 12:10:17 -0400

    removed useless IFS=

--- scripts/createthumbnails.sh
+++ scripts/createthumbnails.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-#give text document of file names, newline seperated
+# Give file of names newline seperated
 
+[ -z "$1" ] && { notify-send "No file given..." ; exit ;}
 
-[ -z "$1" ] && notify-send "No file given..." && exit
-
-
-while IFS= read -r line; do
+while read -r line; do
     title=$(echo "$line" | sed 's/^/thumbnail-/')
-    ffmpeg -nostdin -i "$line" -vf scale=192:108 "$title" #ffmpeg requires the -nostdin flag when used in loop
+    # ffmpeg requires the -nostdin flag when used in loop
+    ffmpeg -nostdin -i "$line" -vf scale=192:108 "$title"
 done < "$1"
--- scripts/replacewebsitepics.sh
+++ scripts/replacewebsitepics.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-# Requires text file with names of pictures that you want, seperated by newlines,
-# gives output that you can paste into your website html.
-
-
+#requires text file with names of pictures that you want, seperated by newlines
+#gives output that you can paste into your website html
 [ -z "$1" ] && notify-send "Please enter text file..." && exit
-
 
 thetext=$(printf "<a href="pics/title1" target"_blank">
             <img src="pics/thumbs/title2">
             </a>")
 
 
-while IFS= read -r line; do
+
+
+while read -r line; do
     realname="$line"
     thumbname=$(echo "$realname" | sed 's/^/thumbnail-/g')
     echo "$thetext" | sed "s/title1/$realname/;s/title2/$thumbname/" >> correctedforwebsite.txt
+
 done < "$1"
