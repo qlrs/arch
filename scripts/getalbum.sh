f3f804dea21b9427a2fc489fef370acdd8955902
jonson mccready <for44jtime@protonmail.com>
2021-10-21 18:29:48 -0400

    cleaned

--- scripts/getalbum.sh
+++ scripts/getalbum.sh
@@ -1,34 +1,21 @@
 #!/bin/sh
-#you need to specify downloadmusic in your youtube-dl config
-#-o "$HOME"/downloadmusic/%(title)s.%(ext)s
+# Might need to switch to youtube-dlp
 
-musiclocation() {
-    [ ! -d "$HOME"/downloadmusic ] && mkdir "$HOME"/downloadmusic
-    cd "$HOME"/downloadmusic || exit
-    mv ./* album
-    mv album "$musicpath"
-    cd "$musicpath" || exit
-    touch times
+[ -z "$1" ] && notify-send "No link given... Exiting" && exit
+
+songinfo() {
+    echo "what would you like the artist directory to be called? "; read -r artistname
+    echo "what would you like the album directory to be called? "; read -r albumname
+    [ -z "$artistname" ] && notify-send "Nothing entered, exiting..." && exit
+    [ -z "$albumname" ] && notify-send "Nothing entered, exiting..." && exit
 }
 
-echo "what would you like the artist directory to be called? "; read -r artistname
-echo "what would you like the album directory to be called? "; read -r albumname
-[ -z "$artistname" ] && notify-send "Nothing entered, exiting..." && exit
-[ -z "$albumname" ] && notify-send "Nothing entered, exiting..." && exit
-[ -z "$1" ] && notify-send "No link found, please enter a link" && exit
+songinfo
 
-if { [ -e "$HOME"/music/"$artistname" ] && [ -e "$HOME"/music/"$artistname"/"$albumname" ]; }; then
+if { [ -e "$HOME/music/$artistname" ] && [ -e "$HOME/music/$artistname/$albumname" ]; }; then
     echo "That already exists"
-elif { [ -e "$HOME"/music/"$artistname" ] && [ ! -e "$HOME"/music/"$artistname"/"$albumname" ]; }; then 
-    mkdir -p "$HOME"/music/"$artistname"/"$albumname"
-    cd "$HOME"/music/"$artistname"/"$albumname" || exit
-    musicpath="$PWD"
-    youtube-dl -f m4a "$1"
-    musiclocation
 else
-    mkdir -p "$HOME"/music/"$artistname"/"$albumname"
-    cd "$HOME"/music/"$artistname"/"$albumname" || exit
-    musicpath=$PWD
-    youtube-dl -f m4a "$1"
-    musiclocation
+    mkdir -p "$HOME/music/$artistname/$albumname"
+    youtube-dl -o "$HOME/music/$artistname/$albumname/album.%(ext)s" --extract-audio --audio-format mp3 "$1"
+    touch "$HOME/music/$artistname/$albumname/times"
 fi
