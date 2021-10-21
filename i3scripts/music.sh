d4e1c49b6b2cce3020bdb690701b67babd6e3b05
jonson mccready <for44jtime@protonmail.com>
2021-10-21 19:32:51 -0400

    added play/pause icon to music module

--- i3scripts/music.sh
+++ i3scripts/music.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if mpc | grep -q "playing"; then
+    echo "⏸️ 🎶"
+elif
+    mpc | grep -q "paused"; then
+    echo "▶️ 🎶"
+else
+    echo "🎶"
+fi
+
 case $BLOCK_BUTTON in
     1) setsid -f "$TERMINAL" -e ncmpcpp;;
     2) mpc clear > /dev/null ;;
