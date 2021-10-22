586619ae1dda45e53e7f8f52f0f69d77595305e0
jonson mccready <for44jtime@protonmail.com>
2021-10-22 19:04:14 -0400

    > /dev/null for case statement

--- i3scripts/music.sh
+++ i3scripts/music.sh
@@ -11,8 +11,8 @@ fi
 
 case $BLOCK_BUTTON in
     1) setsid -f "$TERMINAL" -e ncmpcpp;;
-    2) mpc clear > /dev/null ;;
-    3) mpc toggle > /dev/null ;;
-    4) mpc next > /dev/null ;;
-    5) mpc prev > /dev/null ;;
-esac
+    2) mpc clear ;;
+    3) mpc toggle ;;
+    4) mpc next ;;
+    5) mpc prev ;;
+esac > /dev/null
