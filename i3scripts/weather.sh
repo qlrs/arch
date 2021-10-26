c1c648c5ada42ec5e0c52d0482828b9394bf3159
jonson mccready <for44jtime@protonmail.com>
2021-10-26 11:34:55 -0400

    open wttr.in with right click

--- i3scripts/weather.sh
+++ i3scripts/weather.sh
@@ -7,6 +7,7 @@ curl -s https://wttr.in/Myrtle_Beach\?format=j1 > /tmp/weather_forecast
 getforecast() {
     temp="$(jq -r ."current_condition[0].temp_F" /tmp/weather_forecast)"
     condition="$(jq -r ."current_condition[0]|.weatherDesc[0]|.value" /tmp/weather_forecast)"
+    curl "http://wttr.in/Myrtle_Beach?u" > "$HOME/.cache/weatherforcast"
 }
 
 displayforecast () {
@@ -17,12 +18,12 @@ displayforecast () {
         Rainy) icon="🌧️";;
         *) icon="☀️";;
     esac
-
     echo "$icon $temp℉ "
 }
 
 case $BLOCK_BUTTON in
-    1) $BROWSER "https://forecast.weather.gov/MapClick.php?CityName=Myrtle+Beach&state=SC&site=ILM&textField1=33.6988&textField2=-78.8922" ;;
+    1) "$BROWSER" "https://forecast.weather.gov/MapClick.php?CityName=Myrtle+Beach&state=SC&site=ILM&textField1=33.6988&textField2=-78.8922" ;;
+    3) "$TERMINAL" --hold -e cat "$HOME/.cache/weatherforcast" ;;
 esac
 
 getforecast
