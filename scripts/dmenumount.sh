e6502635914a257f6056bcfbff0c1a6c376a8c05
jonson mccready <for44jtime@protonmail.com>
2021-11-23 12:20:11 -0500

    cleaned

--- scripts/dmenumount.sh
+++ scripts/dmenumount.sh
@@ -1,36 +1,19 @@
 #!/bin/sh
 
-#get a list of mountable drives
-#if no drives listed then exit
-mountabledrives=$(lsblk -lp | awk '/part[[:blank:]]$/ {print $1, "("$4")"}')
-[ -z "$mountabledrives" ] && exit
+drives=$(lsblk -lp | awk '/part[[:blank:]]$/ {print $1, "("$4")"}')
+[ -z "$drives" ] && exit
 
+selec=$(echo "$drives" | dmenu -i -l 5 -p "Enter drive to mount: " | awk '{print $1}')
+[ -z "$selec" ] && notify-send "Nothing selected, exiting..." && exit
 
-#let user select one of the mountable drives
-#if none chosen then exit
-userselection=$(echo "$mountabledrives" | dmenu -i -l 5 -p "Enter drive to mount: " | awk '{print $1}')
-[ -z "$userselection" ] && notify-send "Nothing selected, exiting..." && exit
-getfiletype=$(lsblk -o "fstype" "$userselection" | sed -n '2p')
-[ -z "$userselection" ] && exit
-
-
-#give user a list of possible locations to mount the drive
-#if mount location does not exist then let user create it
-chosenmount=$(printf "/home/dan/storagedrive\n/mnt/usbstick" | dmenu -i -l 5 -p "Choose where to mount the drive: ")
-if [ ! -d "$chosenmount" ]; then
+ml=$(printf "/home/dan/storagedrive\n/mnt/usbstick" | dmenu -i -l 5 -p "Choose where to mount the drive: ")
+if [ ! -d "$ml" ]; then
     newdir=$(printf "Yes\nNo" | dmenu -i -p "Mount point does not exist, create it?")
     if [ "$newdir" = "Yes" ];then
-        sudo -A mkdir -p "$chosenmount"
+        sudo -A mkdir -p "$ml"
     else
         notify-send "Nothing selected, exiting... " && exit
     fi
 fi
 
-
-#mount the drive with mount option "-o umask=0000"
-case "$getfiletype" in
-    "vfat") sudo mount -t  vfat "$userselection" "$chosenmount" -o umask=000 && notify-send "Mounting..." ;;
-    "iso9660") sudo mount -t iso9660 "$userselection" "$chosenmount" -o umask=000 && notify-send "Mounting..." ;;
-    *) sudo mount "$userselection" "$chosenmount" && notify-send "Mounting..." ;;
-esac
-
+sudo mount "$selec" "$ml" -o umask=000 && notify-send "Mounting..."
--- scripts/es.sh
+++ scripts/es.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-du -a "$HOME/scripts" "$HOME/.config" "$HOME/linuxbook" | awk '{print $2}' | fzf | xargs -r "$EDITOR"
-
+du -a "$HOME/scripts" \
+    "$HOME/.config/aerc" \
+    "$HOME/.config/alacritty" \
+    "$HOME/.config/calcurse" \
+    "$HOME/.config/dunst" \
+    "$HOME/.config/i3" \
+    "$HOME/.config/lf" \
+    "$HOME/.config/nvim/init.vim" \
+    "$HOME/.config/nvim/templates" \
+    "$HOME/.config/qutebrowser" \
+    "$HOME/.config/ranger" \
+    "$HOME/.config/sxhkd" | awk '{print $2}' | fzf | xargs -r "$EDITOR"
