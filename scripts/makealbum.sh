0eaa5cb52ab69ec2856917a399d5e020e05fa09d
jonson mccready <for44jtime@protonmail.com>
2021-12-08 18:34:45 -0500

    revamp

--- scripts/makealbum.sh
+++ scripts/makealbum.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-### NEEDS A REWORK ###
 # First argument is album file and second is time file formatted as
 # 00:00:00 song name
 # 00:03:00 song name_2
 
-[ -z "$1" ] && { echo 'First argument is album, second is times' ; exit ;}
-[ -z "$2" ] && { echo 'First argument is album, second is times' ; exit ;}
+{ [ -z "$1" ] || [ -z "$2" ] ;} && { echo 'First argument is album, second is times' ; exit ;}
 
 echo "What is the artist name? " ; read -r artistname
 echo "What is the album name? " ; read -r albumname
@@ -30,38 +28,34 @@ while :; do
 done
 
 counter=1
-while read -r line; do
-    end=$(echo "$line" | cut -d' ' -f1)
+while read -r time song; do
     tracknum=$((counter - 1))
-
     [ -n "$start" ] &&
-        echo "Working on $name" &&
-        [ "$userext" = ".opus" ] && 
-            ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" -to "$end" "$filename" 2> /dev/null && 
-            printf "Artist=$artistname\nAlbum=$albumname\nTitle=$name\nTrack=$tracknum" | opustags -i -S "$filename"
-        [ "$userext" = ".mp3" ] && 
-            ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" -to "$end" "$filename" 2> /dev/null && 
-            eyeD3 -Q --remove-all -a "$artistname" -A "$albumname" -t "$name" -n "$tracknum" "$filename"
-        [ -z "$userext" ] && notify-send "Exiting..." && exit
+        echo "Working on $songname" &&
+        if [ "$userext" = ".mp3" ]; then
+            ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" -to "$time" "$filename" 2> /dev/null && 
+            eyeD3 -Q --remove-all -a "$artistname" -A "$albumname" -t "$songname" -n "$tracknum" "$filename"
+        else
+            ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" -to "$time" "$filename" 2> /dev/null &&
+            printf "Artist=$artistname\nAlbum=$albumname\nTitle=$songname\nTrack=$tracknum" | opustags -i -S "$filename"
+        fi
 
-    start="$end"
-    echo 
-    name=${line##?????????}
-    fakefilename=$(echo "$name" | sed 's/\s/_/g')
-    filename="$fakefilename$userext"
+    start=$time
+    songname=$song
+    filename=$(echo "$songname" | sed 's/\s/_/g' | tr '[:upper:]' '[:lower:]')$userext
     counter=$((counter + 1))
-
 
 done < "$2"
 
-tracknum=$((tracknum +1 ))
-echo "Almost done... Working on $name"
-[ "$userext" = ".opus" ] &&
-    ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" "$filename" 2> /dev/null &&
-    printf "Artist=$artistname\nAlbum=$albumname\nTitle=$name\nTrack=$tracknum" | opustags -i -S "$filename"
-[ "$userext" = ".mp3" ] &&
-    ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" "$filename" 2> /dev/null && 
-    eyeD3 -Q --remove-all -a "$artistname" -A "$albumname" -t "$name" -n "$tracknum" "$filename"
+tracknum=$((tracknum + 1 ))
+echo "Almost done... Working on $songname"
+    if [ "$userext" = ".mp3" ]; then
+        ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" "$filename" 2> /dev/null && 
+        eyeD3 -Q --remove-all -a "$artistname" -A "$albumname" -t "$songname" -n "$tracknum" "$filename"
+    else
+        ffmpeg -nostdin -i "$1" -b:a 320k -ss "$start" "$filename" 2> /dev/null &&
+        printf "Artist=$artistname\nAlbum=$albumname\nTitle=$songname\nTrack=$tracknum" | opustags -i -S "$filename"
+    fi
 
 rm "$1"
 rm "$2"
