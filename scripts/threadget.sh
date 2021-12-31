64b6201cd76337da6eaf22dd27bcfb25ff0c7d94
jonson mccready <for44jtime@protonmail.com>
2021-12-31 18:48:44 -0500

    working now

--- scripts/threadget.sh
+++ scripts/threadget.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# needs 'https://' in front
-thread=$(curl "$1" | grep -o 'i\.4cdn\.org\/wg\/[[:digit:]]*\.\(jpg\|png\|jpeg\)')
-
-firefox "https://$thread"
+for i in $(curl "$1" | grep -o 'i\.4cdn\.org\/wg\/[[:digit:]]*\.\(jpg\|png\|jpeg\)'); do
+    wget -nc "$i"
+done
