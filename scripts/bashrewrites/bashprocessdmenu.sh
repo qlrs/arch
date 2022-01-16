24be9bd49bc473dcaadfbc44b11b76ec96a8064a
jonson mccready <for44jtime@protonmail.com>
2022-01-16 14:53:34 -0500

    initial

---
+++ scripts/bashrewrites/bashprocessdmenu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chosen=$(ps -axco pid,comm | dmenu -l 15 -p 'What process to kill? ' -i)
+[ -z "$chosen" ] && { notify-send 'Nothing selected' ; exit 1 ;}
+
+process=${chosen% *}
+process=${process// /}
+kill "$process" && notify-send "$process... W A S T E D"
