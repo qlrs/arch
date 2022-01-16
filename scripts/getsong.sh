bd0f6ac3abe713169339b2d97e7fe19190d29096
jonson mccready <for44jtime@protonmail.com>
2022-01-15 19:14:19 -0500

    cleaned

--- scripts/getsong.sh
+++ scripts/getsong.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-# Might need to switch to youtube-dlp to uncap download limit
-
 [ -z "$1" ] && { echo 'No link given... exiting.' ; exit 1 ;}
 
 songinfo() {
     echo 'Artist directory name? '; read -r dirartistname
     echo 'Song file name? '; read -r filename
     echo 'Artist name? ' ; read -r artistname
-    echo 'Song name?' ; read -r songname
+    echo 'Song name? ' ; read -r songname
+    musicpath=$HOME/music/$dirartistname
 }
 
 processsong() {
-    musicpath="$HOME"/music/"$dirartistname"
     yt-dlp -o "$musicpath/$filename.%(ext)s" \
         --extract-audio --audio-format mp3 "$@"
     #cd "$musicpath" || exit
     eyeD3 -Q --remove-all -a "$artistname" -t "$songname" \
-        "$musicpath"/"$filename.mp3"
+        "$musicpath/$filename.mp3"
 }
 
 songinfo
 
-mkdir -p "$HOME"/music/"$dirartistname" ; processsong "$1"
+mkdir -p "$musicpath" && processsong "$1"
