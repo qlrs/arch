e7031852fd30b9b7af3ec81e240f198bb122b023
jonson mccready <for44jtime@protonmail.com>
2022-01-15 19:22:15 -0500

    initial

---
+++ scripts/bashrewrites/bashgetsong.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+[ -z "$1" ] && { echo 'No link given... exiting.' ; exit 1 ;}
+
+songinfo() {
+    read -p 'Artist name? ' -r artistname
+    dirartistname=${artistname,,}
+    dirartistname=${dirartistname// /_}
+
+    read -p 'Song name? ' -r songname
+    filename=${songname,,}
+    filename=${filename// /_}
+
+    musicpath=$HOME/music/$dirartistname
+}
+
+processsong() {
+    yt-dlp -o "$musicpath/$filename.%(ext)s" \
+        --extract-audio --audio-format mp3 "$@"
+    eyeD3 -Q --remove-all -a "$artistname" -t "$songname" \
+        "$musicpath/$filename.mp3"
+}
+
+songinfo
+
+mkdir -p "$musicpath" && processsong "$1"
