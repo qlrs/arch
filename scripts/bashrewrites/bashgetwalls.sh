627b8bc352a1e7bfa06668c29ff5005e15a44218
jonson mccready <for44jtime@protonmail.com>
2022-01-22 18:00:49 -0500

    initial

---
+++ scripts/bashrewrites/bashgetwalls.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Give url of picture or list with $url $name
+[ -z "$1" ] && { printf "Please enter a url or list\n" ; exit 1 ;}
+
+getone() {
+    read -p 'What do you want the file to be called? ' -r name
+    while true ; do
+    if [ -e "$HOME/stuff/walls/$name" ]; then
+        read -p 'That file exists, please give another name ' -r name
+    else
+        wget -q "$1" -O "/$HOME/stuff/walls/$name" 2> /dev/null
+        printf "Downloaded %s as %s\n" "$1" "$name"
+        break
+    fi
+    done
+}
+
+getmultiple() {
+    while read -r url name; do
+        while true ; do
+            if [ -e "/$HOME/stuff/walls/$name" ] ; then
+                printf "The file %s exists, please change the name.\n" "$name"
+                break
+            else
+                wget -q "$url" -O "/$HOME/stuff/walls/$name" 2> /dev/null
+                break
+            fi
+        done
+    done < "$1"
+}
+
+if [[ $1 = http* ]]; then
+    getone "$@"
+else
+    getmultiple "$@"
+fi
