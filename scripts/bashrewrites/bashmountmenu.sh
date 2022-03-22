c6b31265ae83f6fb7f550514eb1db6293afaee9c
jonson mccready <for44jtime@protonmail.com>
2022-03-22 11:14:59 -0400

    initial

---
+++ scripts/bashrewrites/bashmountmenu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+show_drives()
+{
+while read -r a b c d e f; do
+    if [[ $f =~ part ]]; then
+        printf '%s %-15s (%s)\n' "$a" "$f" "$d"
+    fi
+done < <(lsblk -lp)
+}
+
+mount_drive()
+{
+    location=$(printf "$HOME/storagedrive\n$HOME/usbstick" | dmenu -i -l 5 -p 'Choose mount point or type one')
+    [ -z "$location" ] && exit 2
+    [ ! -d "$location" ] && mkdir -p "$location"
+    sudo mount "$selection" "$location"
+    notify-send "Mounting $selection to $location"
+}
+
+selection=$(show_drives | dmenu -i -l 5 -p 'Enter drive to mount')
+[ -z "$selection" ] && exit 1
+selection=${selection%% *}
+
+mount_drive
