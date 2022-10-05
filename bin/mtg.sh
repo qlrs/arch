d6d8f3d8a9ca85779d7795350d8f94ab0ef48491
laptop <>
2022-10-05 16:27:25 -0400

    cya

--- bin/mtg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-selection="$(du -a "$HOME"/scripts/ "$HOME"/.config/i3 | awk '{print $2}' | fzf)"
-target="$(du --exclude=.git "$HOME"/archconfig/ | awk '{print $2}' | fzf)/"
-cp -iv "$selection" "$target"
--- bin/threadget.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for i in $(curl "$1" | grep -o 'i\.4cdn\.org\/wg\/[[:digit:]]*\.\(jpg\|png\|jpeg\)'); do
-    wget -nc "$i" -P "$HOME/threadwalls"
-done
