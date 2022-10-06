67894e20d562b4026a8eca3f81a434814e6315d2
jonson mccready <for44jtime@protonmail.com>
2022-10-06 14:48:10 -0400

    init

---
+++ config/nvim/templates/skeleton_cli.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="TODO"
+    )
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
