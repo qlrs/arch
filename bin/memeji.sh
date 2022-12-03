45280008563fe420950adc8136493450530da591
laptop <dan@arch.localdomain>
2022-12-03 09:40:41 -0500

    fixed broken path

--- bin/memeji.sh
+++ bin/memeji.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-chosenone=$(cut -d ';' -f1 ~/scripts/emojilist | dmenu -i -l 30 | sed "s/ .*//")
+chosenone=$(cut -d ';' -f1 ~/bin/emojilist | dmenu -i -l 30 | sed "s/ .*//")
 [ -z "$chosenone" ] && { notify-send "Nothing selected... exiting" ; exit ;}
 echo "$chosenone" | tr -d '\n' | xclip &
