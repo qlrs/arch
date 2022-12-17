26c433ded6ccb6622ca3ff2c371116abcdd00da3
laptop <dan@arch.localdomain>
2022-12-17 12:10:41 -0500

    added notification for low bat

--- config/i3scripts/battery.sh
+++ config/i3scripts/battery.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-c="$(cat /sys/class/power_supply/BAT0/capacity)"
-s="$(cat /sys/class/power_supply/BAT0/status)"
+c="$(cat /sys/class/power_supply/BAT1/capacity)"
+s="$(cat /sys/class/power_supply/BAT1/status)"
 
-[ $c -lt 21 ] && echo "⚠️🔋 ${s%??????}- $c%" || echo "🔋 ${s%??????}- $c%"
+# [ "$c" -lt 21 ] && echo "⚠️🔋 ${s%??????}- $c%" || echo "🔋 ${s%??????}- $c%"
+# [ "$c" -lt 21 ] && 
+
+if [ "$c" -lt 21 ]; then
+    echo "⚠️🔋 ${s%??????}- $c%"
+    notify-send "⚠️ Battery $c% ⚠️"
+else
+    echo "🔋 ${s%??????}- $c%"
+fi
+
