f8753c2697779f9a1e6a105605ce501a62c1186e
laptop <dan@arch.localdomain>
2022-12-29 18:17:44 -0500

    removed notification for nothing selected

--- bin/memeji.sh
+++ bin/memeji.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 chosenone=$(cut -d ';' -f1 ~/bin/emojilist | dmenu -i -l 30 | sed "s/ .*//")
-[ -z "$chosenone" ] && { notify-send "Nothing selected... exiting" ; exit ;}
-echo "$chosenone" | tr -d '\n' | xclip &
+[ -z "$chosenone" ] && exit
+echo "$chosenone" | tr -d '\n' | xclip
