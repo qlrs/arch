4c79ee84345f3f9bcf3dbaa245c08919535ceb1a
laptop <dan@arch.localdomain>
2023-01-09 20:24:57 -0500

    removed old comments

--- config/i3scripts/stockprice.sh
+++ config/i3scripts/stockprice.sh
@@ -2,7 +2,3 @@
 
 price=$(curl -s https://query1.finance.yahoo.com/v7/finance/quote\?symbols\=amd | sed 's/,/\n/g' | grep "regularMarketPrice" | cut -d ':' -f2)
 printf "💲AMD: %.05s\n" "$price"
-
-# case $BLOCK_BUTTON in
-#     1) "$TERMINAL" -e mop ;;
-# esac
