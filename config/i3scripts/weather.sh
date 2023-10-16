8274f58f5541682537fc1254f4e680c306af0d91
RS <for44jtime@protonmail.com>
2023-10-16 13:26:44 -0400

    use nws for weather now

--- config/i3scripts/weather.sh
+++ config/i3scripts/weather.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-curl -s https://api.weather.gov/gridpoints/ILM/57,39/forecast > /tmp/weather_forecast
-
-getforecast() {
-    temp=$(jq -r ."properties.periods[0].temperature" /tmp/weather_forecast)
-    condition=$(jq -r ."properties.periods[0].shortForecast" /tmp/weather_forecast)
-}
-getforecast
-echo "☀️ $temp℉ "
+nws --statusbar
 
 case $BLOCK_BUTTON in
-    1) "$BROWSER" "https://forecast.weather.gov/MapClick.php?CityName=Myrtle+Beach&state=SC&site=ILM&textField1=33.6988&textField2=-78.8922" ;;
-    3) notify-send $condition ;;
+  1) nws --r;;
+  3) notify-send -t 8000 "$(nws --hourly)";;
 esac
-
